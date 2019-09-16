72fbcfc64b1e7fd2efb54dbc0cf1cc67eddada56
berend109 <berend109@gmail.com>
2019-09-16 10:30:47 +0200

    seperate the introduction from the game. made first steps for connection with the database.

---
+++ assets/php/conn.php
@@ -0,0 +1,30 @@
+<?php
+class connection {
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
+    private $charset;
+    public function connect() {
+        
+        $this->servername = "localhost";
+        $this->username = "root";
+        $this->password = "";
+        $this->dbname = "keuzedeel(basisGames)";
+        $this->charset = "utf8mb4";
+        try {
+            $dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname.";charset=".$this->charset;
+            $pdo = new PDO($dsn, $this->username, $this->password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            echo "Connection failed: ".$e->getMessage();
+        }
+    }
+}
+
+// test connection
+// echo "<br/>";
+// echo "<br/>";
+// $object = new connection; // class
+// $object->connect(); // function
