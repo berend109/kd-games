dc56ec353341e3cd1e305ff5586c749e407e50e6
berend109 <berend109@gmail.com>
2019-10-23 18:25:33 +0200

    fresh install os created db again.

--- assets/php/conn.php
+++ assets/php/conn.php
@@ -10,12 +10,13 @@ class connection {
         $this->servername = "localhost";
         $this->username = "root";
         $this->password = "";
-        $this->dbname = "keuzedeel(basisGames)";
+        $this->dbname = "kdBasisGames";
         $this->charset = "utf8mb4";
         try {
             $dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname.";charset=".$this->charset;
             $pdo = new PDO($dsn, $this->username, $this->password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connection successfull";
             return $pdo;
         } catch (PDOException $e) {
             echo "Connection failed: ".$e->getMessage();
