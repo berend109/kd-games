7c1abd9f04c71f018c263242789ba1a3af490aa4
berend109 <berend109@gmail.com>
2019-10-23 20:41:03 +0200

    added some prototype code. and a todo in css.

--- assets/JS/binair.js
+++ assets/JS/binair.js
@@ -1,1 +1,19 @@
 // alert ("Binair");
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var question = getRandomInt(10, 20);
+
+alert (question);
+
+var num = prompt("Enter num");
+
+if (question == num) {
+    alert("yes");
+} else {
+    alert("no");
+}
